066b3502072445f61eb4b924ebfa0a2de669cda1
AmirHossein <amir.1381.azhdarnzhad@gmail.com>
2023-06-19 15:10:12 +0330

    Implement basic movements

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,7 +16,8 @@ fn main() {
                 is_over,
                 get_turn,
                 do_a_move,
-                get_possible_moves
+                get_possible_moves,
+                fen
             ])
             .run(tauri::generate_context!())
             .expect("error while running tauri application");
@@ -40,23 +41,36 @@ unsafe fn get_turn() -> char {
 }
 
 #[tauri::command]
-unsafe fn do_a_move(source: &str, destination: &str, promotion: &str) {
-    //first check the possibility
-    //then act
+unsafe fn do_a_move(source: &str, destination: &str, promotion: &str) -> bool {
+    let m = ChessMove::new(
+        Square::from_str(source).unwrap(),
+        Square::from_str(destination).unwrap(),
+        None,
+    );
 
+    let mut new_game = GAME.clone().unwrap();
+
+    let result = new_game.make_move(m);
+    GAME = Some(new_game);
+
+    return result;
 }
 
 #[tauri::command]
-unsafe fn get_possible_moves(square: &str) -> Vec<ChessMove> {
-
+unsafe fn get_possible_moves(square: &str) -> Vec<String> {
     let square = Square::from_str(square).unwrap();
-    let mut possible_moves: Vec<ChessMove> = vec![];
+    let mut possible_moves: Vec<String> = vec![];
 
     for possible_move in MoveGen::new_legal(&GAME.clone().unwrap().current_position()) {
         if possible_move.get_source() == square {
-            possible_moves.push(possible_move);
+            possible_moves.push(possible_move.to_string());
         }
     }
 
     return possible_moves;
+}
+
+#[tauri::command]
+unsafe fn fen() -> String {
+    return GAME.clone().unwrap().current_position().to_string();
 }
